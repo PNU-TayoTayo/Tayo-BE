6718a8d89253857518c725ff138db2832d997690
kimdonwoo <ehstjstod@gmail.com>
2023-06-15 21:21:35 +0900

    - initial commit
    setting project

---
+++ Tayo-Backend/src/main/java/pnu/cse/TayoTayo/TayoBE/TayoBeApplication.java
@@ -0,0 +1,13 @@
+package pnu.cse.TayoTayo.TayoBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TayoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TayoBeApplication.class, args);
+	}
+
+}
